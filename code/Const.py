6021ba2b020dabeb6a40bff5a18f0be2929c9b49
Tiago_Mendes_PC-CASA <tiago.mendes.goes@gmail.com>
2024-12-08 11:00:14 -0300

    Creating Player Shoot Creating Enemy Shoot

--- code/Const.py
+++ code/Const.py
@@ -8,6 +8,13 @@ COLOR_YELLOW = (253, 208, 23)
 # E
 ENEMY_SPAWN_TIME = 3000
 
+ENTITY_SHOOT_DELAY = {
+    'Player1Shoot':20,
+    'Player2Shoot': 40,
+    'Enemy1Shoot': 80,
+    'Enemy2Shoot': 120
+}
+
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
     'Level1Bg1': 999,
@@ -19,7 +26,11 @@ ENTITY_HEALTH = {
     'Player1': 300,
     'Player2': 300,
     'Enemy1': 300,
-    'Enemy2': 300
+    'Enemy2': 300,
+    'Player1Shoot': 999,
+    'Player2Shoot': 999,
+    'Enemy1Shoot': 999,
+    'Enemy2Shoot': 999,
 }
 
 ENTITY_SPEED = {
@@ -31,9 +42,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 6,
     'Level1Bg6': 8,
     'Player1': 4,
+    'Player1Shoot': 1,
     'Player2': 4,
+    'Player2Shoot': 1,
     'Enemy1': 2,
-    'Enemy2': 1
+    'Enemy1Shoot': 3,
+    'Enemy2': 1,
+    'Enemy2Shoot': 2,
 }
 
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -50,7 +65,7 @@ PLAYER_KEY_UP = {'Player1': pygame.K_UP, 'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN, 'Player2': pygame.K_s}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT, 'Player2': pygame.K_d}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT, 'Player2': pygame.K_a}
-PLAYER_KEY_SHOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
 
 # W
 WIN_WIDTH = 576
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,8 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShoot import EnemyShoot
 from code.Entity import Entity
+from code.PlayerShoot import PlayerShoot
 
 
 class EntityMediator:
@@ -8,6 +11,14 @@ class EntityMediator:
     def __verify_collision_window(entity: Entity):
             if isinstance(entity, Enemy):
                 if entity.rect.right < 0:
+                    entity.health = 0
+
+            if isinstance(entity,PlayerShoot):
+                if entity.rect.left >= WIN_WIDTH:
+                    entity.health = 0
+
+            if isinstance(entity,EnemyShoot):
+                if entity.rect.right <= 0:
                     entity.health = 0
 
     @staticmethod
