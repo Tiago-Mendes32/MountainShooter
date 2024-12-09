afc353b6b0d4ff0ce840d0da5ecdfff42edcabca
Tiago_Mendes_PC-CASA <tiago.mendes.goes@gmail.com>
2024-12-09 03:58:38 -0300

    Score creating

---
+++ code/Score.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import C_YELLOW, SCORE_POS
+
+
+class Score:
+    def __init__(self, window: Surface):
+        self.window = window
+        self.surf = pygame.image.load('./asset/ScoreBg.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def save(self, menu_return: str, player_score: list[int]):
+        pygame.mixer_music.load('./asset/Score.mp3')
+        pygame.mixer_music.play(-1)
+        self.window.blit(source=self.surf, dest=self.rect)
+        while True:
+            pygame.display.flip()
+            self.score_text(48, f'YOU WIN!: Player1 Score{player_score[0]}', C_YELLOW, SCORE_POS['Title'])
+            pass
+
+    def show(self):
+        pygame.mixer_music.load('./asset/Score.mp3')
+        pygame.mixer_music.play(-1)
+        self.window.blit(source=self.surf, dest=self.rect)
+        while True:
+            pygame.display.flip()
+            pass
+
+    def score_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
